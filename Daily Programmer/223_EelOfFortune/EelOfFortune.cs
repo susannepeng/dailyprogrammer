6bcbbf219c28b9991191d557c96ac881e01d916a
Susanne Peng <susanne.peng@gmail.com>
2015-07-17 13:58:28 +1000

    223: Eel of Fortune

---
+++ Daily Programmer/223_EelOfFortune/EelOfFortune.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Programmer._223_EelOfFortune
+{
+    public class EelOfFortune
+    {
+        public void GetChallengeResults()
+        {
+            Console.WriteLine(Problem("synchronized", "snond"));
+            Console.WriteLine(Problem("misfunctioned", "snond"));
+            Console.WriteLine(Problem("mispronounced", "snond"));
+            Console.WriteLine(Problem("shotgunned", "snond"));
+            Console.WriteLine(Problem("snond", "snond"));
+        }
+
+        public bool Problem(string targetWord, string badWord)
+        {
+            var lib = badWord.ToCharArray().Distinct();
+            var reveal = new string(
+                    targetWord.ToCharArray()
+                    .Where(c => lib.Contains(c))
+                    .ToArray()
+                );
+
+            return reveal == badWord;
+        }
+    }
+}
